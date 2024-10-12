e70a3cde2089e56b8977a9c616690c95b7ecb9a5
Brennan Ngo(Anh Ngo) <anh.nh182113@gmail.com>
2024-10-12 10:28:38 +0700

    update

--- src/contracts/address/hashield-ai.ts
+++ src/contracts/address/hashield-ai.ts
@@ -1,10 +1,10 @@
 import { ChainId, Web3Address } from 'src/app.type';
 
 export const hashieldAIAddresses: Record<ChainId, Web3Address> = {
-  [ChainId.BinanceSmartChain]: '0x1e33E4C40B892Cc38045dCD82d522B2ed5b3Baa9',
-  [ChainId.Arbitrum]: '0x1e33E4C40B892Cc38045dCD82d522B2ed5b3Baa9',
-  [ChainId.Ethereum]: '0x1e33E4C40B892Cc38045dCD82d522B2ed5b3Baa9',
-  [ChainId.Polygon]: '0x1e33E4C40B892Cc38045dCD82d522B2ed5b3Baa9',
+  [ChainId.BinanceSmartChain]: '0x7D54fa007bD58F4fff49aDeD96eC93CBf6055a9D',
+  [ChainId.Arbitrum]: '0x7D54fa007bD58F4fff49aDeD96eC93CBf6055a9D',
+  [ChainId.Ethereum]: '0x7D54fa007bD58F4fff49aDeD96eC93CBf6055a9D',
+  [ChainId.Polygon]: '0x7D54fa007bD58F4fff49aDeD96eC93CBf6055a9D',
   [ChainId.BinanceSmartChainTestNet]:
     '0x1e33E4C40B892Cc38045dCD82d522B2ed5b3Baa9',
 };
--- src/contracts/address/subscription.ts
+++ src/contracts/address/subscription.ts
@@ -1,10 +1,10 @@
 import { ChainId, Web3Address } from 'src/app.type';
 
 export const subscriptionAddresses: Record<ChainId, Web3Address> = {
-  [ChainId.BinanceSmartChain]: '0x1B06453e00Ee1ebE8c63B23596ebbE1006D0Ea50',
-  [ChainId.Arbitrum]: '0x1B06453e00Ee1ebE8c63B23596ebbE1006D0Ea50',
-  [ChainId.Ethereum]: '0x1B06453e00Ee1ebE8c63B23596ebbE1006D0Ea50',
-  [ChainId.Polygon]: '0x1B06453e00Ee1ebE8c63B23596ebbE1006D0Ea50',
+  [ChainId.BinanceSmartChain]: '0xA464a361037DEDbaED363CcFAa6ec26cA928Db0c',
+  [ChainId.Arbitrum]: '0xA464a361037DEDbaED363CcFAa6ec26cA928Db0c',
+  [ChainId.Ethereum]: '0xA464a361037DEDbaED363CcFAa6ec26cA928Db0c',
+  [ChainId.Polygon]: '0xA464a361037DEDbaED363CcFAa6ec26cA928Db0c',
   [ChainId.BinanceSmartChainTestNet]:
     '0x1B06453e00Ee1ebE8c63B23596ebbE1006D0Ea50',
 };
